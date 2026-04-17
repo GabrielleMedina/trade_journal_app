82378c178628677c67c85ba0883a226dcd644442
GabrielleMedina <157821017+GabrielleMedina@users.noreply.github.com>
2026-04-17 16:20:48 -0600

    Update - Added POST method for new entry

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
+import uuid
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -12,7 +13,7 @@ class JournalEntry(db.Model):
     amount_risked =  db.Column(db.Float, default = 0)
     pnl = db.Column(db.Float, default = 0)
     portfolio_change = db.Column(db.Float, default = 0)
-    result = db.Column(db.String(20), default = 0)
+    result = db.Column(db.String(20), default="Unknown")
     notes = db.Column(db.Text, nullable=False)
 
     def __repr__(self):
@@ -22,7 +23,40 @@ class JournalEntry(db.Model):
 @app.route("/")
 def index():
     return render_template('base.html')
-    
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route("/entries")
+def entries():
+    return render_template("entries.html")
+
+    
+@app.route("/new_entry", methods=['POST', 'GET'])
+def new_entry():
+    if request.method == 'POST':
+        form_date = datetime.strptime(request.form['date'], "%Y-%m-%d").date()
+        form_amount_risked = float(request.form['amount_risked'])
+        form_pnl = float(request.form['pnl'])
+        form_portfolio_change = float(request.form['portfolio_change'])
+        form_result = request.form['result'].strip()
+        form_notes = request.form['notes'].strip()
+        
+        new_entry = JournalEntry(
+            date = form_date,
+            amount_risked = form_amount_risked,
+            pnl = form_pnl,
+            portfolio_change = form_portfolio_change,
+            result = form_result,
+            notes = form_notes
+        )
+        try: 
+            db.session.add(new_entry)
+            db.session.commit()
+            return redirect('/new_entry')
+        except:
+            return 'There was an issue adding your new entry.'
+    return render_template("new_entry.html")
+
+
