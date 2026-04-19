2e61c4c1a34d37f16e5de37cd9fa28d7f3106911
GabrielleMedina <157821017+GabrielleMedina@users.noreply.github.com>
2026-04-19 16:05:21 -0600

    Update - Finished basic update logic,

--- app.py
+++ app.py
@@ -59,11 +59,40 @@ def new_entry():
             return 'There was an issue adding your new entry.'
     return render_template("new_entry.html")
 
-@app.route('/edit_entry/<int:entry_id>')
+@app.route("/edit_entry/<int:entry_id>", methods=["GET", "POST"])
 def edit_entry(entry_id):
-    return redirect(url_for("entries"))
+    entry = db.get_or_404(JournalEntry, entry_id)
+    if request.method == 'POST':
+        form_date = datetime.strptime(request.form['date'], "%Y-%m-%d").date()
+        form_amount_risked = float(request.form['amount_risked'])
+        form_pnl = float(request.form['pnl'])
+        form_portfolio_change = float(request.form['portfolio_change'])
+        form_result = request.form['result'].strip()
+        form_notes = request.form['notes'].strip()
+        
+        
+        entry.date = form_date
+        entry.amount_risked = form_amount_risked
+        entry.pnl = form_pnl
+        entry.portfolio_change = form_portfolio_change
+        entry.result = form_result
+        entry.notes = form_notes
+        try: 
+            db.session.commit()
+            return redirect(url_for("entries"))
+        except:
+            return 'There was an issue editing your new entry.'
+    return render_template("edit_entry.html", entry=entry)
+
+
+
 
 @app.route('/delete_entry/<int:entry_id>', methods=["POST"])
 def delete_entry(entry_id):
-    return redirect(url_for("entries"))
-
+    entry = db.get_or_404(JournalEntry, entry_id)
+    try: 
+        db.session.delete(entry)
+        db.session.commit()
+        return redirect(url_for("entries"))
+    except:
+        return 'There was an issue removing your entry.'
